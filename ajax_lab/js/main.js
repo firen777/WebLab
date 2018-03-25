4516a8eff3a28c92929d42c6287d05316c38f028
firen777 <hivemindac1995@gmail.com>
2018-03-25 13:03:23 -0700

    AJAX Lab init

---
+++ ajax_lab/js/main.js
@@ -0,0 +1,20 @@
+var myCat = {
+    "name" : "Meow",
+    "species" : "cat",
+    "favFood" : "Tuna",
+};
+
+var myFavColor = ["blue", "green", "purple"];
+
+var thePets = [
+    {
+        "name" : "Meow",
+        "species" : "cat",
+        "favFood" : "Tuna",
+    }, 
+    {
+        "name" : "Bark",
+        "species" : "dog",
+        "favFood" : "carrots",
+    }, 
+]
