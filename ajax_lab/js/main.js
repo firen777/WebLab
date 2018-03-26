4befcd52c20b4e2a453b0471374d86272dff1a7e
firen777 <hivemindac1995@gmail.com>
2018-03-26 01:48:09 -0700

    some ajax lab work

--- ajax_lab/js/main.js
+++ ajax_lab/js/main.js
@@ -1,20 +1,30 @@
-var myCat = {
-    "name" : "Meow",
-    "species" : "cat",
-    "favFood" : "Tuna",
+// var myCat = {
+//     "name" : "Meow",
+//     "species" : "cat",
+//     "favFood" : "Tuna",
+// };
+
+// var myFavColor = ["blue", "green", "purple"];
+
+// var thePets = [
+//     {
+//         "name" : "Meow",
+//         "species" : "cat",
+//         "favFood" : "Tuna",
+//     }, 
+//     {
+//         "name" : "Bark",
+//         "species" : "dog",
+//         "favFood" : "carrots",
+//     }, 
+// ]
+
+var ourRequest = new XMLHttpRequest();
+ourRequest.open("GET", "https://learnwebcode.github.io/json-example/animals-1.json");
+ourRequest.onload = function (){
+    // console.log(ourRequest.responseText);
+    var ourData = ourRequest.responseText;
+    
 };
 
-var myFavColor = ["blue", "green", "purple"];
-
-var thePets = [
-    {
-        "name" : "Meow",
-        "species" : "cat",
-        "favFood" : "Tuna",
-    }, 
-    {
-        "name" : "Bark",
-        "species" : "dog",
-        "favFood" : "carrots",
-    }, 
-]
+ourRequest.send();
